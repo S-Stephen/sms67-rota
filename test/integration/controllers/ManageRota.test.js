feec3d5a5a9250e7fc30388b8858470db9279da7
S-Stephen <stephen.shorrock@gmail.com>
2017-01-25 11:02:46 +0000

    added test manager can edit

--- test/integration/controllers/ManageRota.test.js
+++ test/integration/controllers/ManageRota.test.js
@@ -6,7 +6,7 @@ describe('RotaManagementController', function() {
   //1 Test that we can log in as a manager and create a rota
   //2 That that rota then exists in the new database
 
-  describe('#manager_create_rota',function(){
+  describe('#manager_create_edit_rota',function(){
     it('create a new rota',function (done){
       var agent =  request.agent(sails.hooks.http.app);
 
@@ -27,16 +27,29 @@ describe('RotaManagementController', function() {
                  {rot_code: "zzz", rot_description: "some rota"}
              ).expect(201)
               .end(function(err,res){
-                  if (err){ 
+                    if (err){ 
 			              sails.log.debug(" there was an error adding a new rota "+err); 
 			              return done(err);
-		              }
+		            }
                     Rota.findOne({rot_code:'zzz'})
-                      .then(function(testRota) {
+                    .then(function(testRota) {
                       //console.log(JSON.stringify(testUser))
                         assert.equal(true,!!testRota,"a new rota has been created")
-                        done();
-                      }).catch(done);
+                        // Now test edit rota:
+                        agent
+                        .put('/manager/rota/edit')
+                        .send(
+                            {id: testRota.id, rot_code:"zzz", rot_description:"changed rota"}
+                        )
+                        .expect(201)
+                        .end(function(err,res){
+                            if (err){
+                                sails.log.debug("there was an error editing the rota"+err)
+                                return done(err)
+                            }
+                            done()
+                        })                      
+                    }).catch(done);
              })
         })        
     }) 
