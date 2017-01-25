509df878dcebb530f1c8d6a2b117a22da9558d1b
S-Stephen <stephen.shorrock@gmail.com>
2017-01-25 15:01:03 +0000

    Test that a non-mananger cannot remove a session

--- test/integration/controllers/ManageSessions.test.js
+++ test/integration/controllers/ManageSessions.test.js
@@ -50,11 +50,14 @@ describe('ManageSessionsController', function() {
   describe('#nonmanager_create_session',function(){
     it('test that a non-manager CANNOT create a new session',function (done){
     //sails.log.debug("Test sessions controller");
+
+
     var agent =  request.agent(sails.hooks.http.app);
-    sessiondate = new Date()
+    
+    //first create a new session (by a manager) to attempt to remove later
     var mreq = agent
     .get('/auth/bearer')
-    .set('authorization','Bearer silverticket')
+    .set('authorization','Bearer goldenticket')
     .redirects(1)
     .expect(200)
     .end(function(err,res){
@@ -63,17 +66,57 @@ describe('ManageSessionsController', function() {
 		    sails.log.debug("Authorization failed"); 
 		    return done(err) 
 	    }
+
         agent
         .post('/manager/schedule/add')
         .send(
-            {scd_user_username: "test0002", scd_date: sessiondate, scd_rota_code: 'EVE'}
-        ).expect(403)
+            {scd_user_username: "test0002", scd_date: sessiondate, scd_rota_code: 'MOR'}
+        ).expect(201)
         .end(function(err,res){
             if (err){ 
 			    sails.log.debug(" There was an error adding a new session "+err); 
 			    return done(err);
 		    }
-            done()
+            assert.equal(true,!!JSON.parse(res.text).id,"a test session has been created")
+            newschedid = JSON.parse(res.text).id
+
+
+            sessiondate = new Date()
+            var mreq = agent
+            .get('/auth/bearer')
+            .set('authorization','Bearer silverticket')
+            .redirects(1)
+            .expect(200)
+            .end(function(err,res){
+                if (err){
+		            sails.log.debug("Authorization failed"); 
+		            return done(err) 
+	            }
+                agent
+                .post('/manager/schedule/add')
+                .send(
+                    {scd_user_username: "test0002", scd_date: sessiondate, scd_rota_code: 'LEV'}
+                ).expect(403)
+                .end(function(err,res){
+                    if (err){ 
+			            sails.log.debug(" There was an error adding a new session "+err); 
+			            return done(err);
+		            }
+
+                    //we are now logged in as a non-manager user
+                    //also test that we are unable to remove the session created earlier by the manager
+                    agent
+                    .delete('/manager/schedule/del/'+newschedid)
+                    .expect(403)
+                    .end(function(err,res){
+                        if (err){ 
+			                sails.log.debug(" There was an error removing the new session "+err); 
+			                return done(err);
+		                }
+                        done()  
+                    })
+                })
+            })
         })
     })
     })
