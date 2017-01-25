4f270e6f54aa15e7754e563db7874b76d7d64def
S-Stephen <stephen.shorrock@gmail.com>
2017-01-25 10:21:51 +0000

    added tests for rota creation

---
+++ test/integration/controllers/ManageRota.test.js
@@ -0,0 +1,74 @@
+var assert = require("assert");
+var request = require('supertest');
+var session = require('supertest-session');
+
+describe('RotaManagementController', function() {
+  //1 Test that we can log in as a manager and create a rota
+  //2 That that rota then exists in the new database
+
+  describe('#manager_create_rota',function(){
+    it('create a new rota',function (done){
+      var agent =  request.agent(sails.hooks.http.app);
+
+      var mreq = agent
+        .get('/auth/bearer')
+        .set('authorization','Bearer goldenticket')
+        .redirects(1)
+        .expect(200)
+       //.get(myapp)
+        .end(function(err,res){
+            if (err){
+		          sails.log.debug("Authorization failed"); 
+		          return done(err) 
+	          }
+            agent
+            .post('/manager/rota/add/')
+            .send(
+                 {rot_code: "zzz", rot_description: "some rota"}
+             ).expect(201)
+              .end(function(err,res){
+                  if (err){ 
+			              sails.log.debug(" there was an error adding a new rota "+err); 
+			              return done(err);
+		              }
+                    Rota.findOne({rot_code:'zzz'})
+                      .then(function(testRota) {
+                      //console.log(JSON.stringify(testUser))
+                        assert.equal(true,!!testRota,"a new rota has been created")
+                        done();
+                      }).catch(done);
+             })
+        })        
+    }) 
+  }) 
+  describe('#nonmanager_create_rota',function(){
+    it('create a new rota',function (done){
+      var agent =  request.agent(sails.hooks.http.app);
+
+      var mreq = agent
+        .get('/auth/bearer')
+        .set('authorization','Bearer silverticket')
+        .redirects(1)
+        .expect(200)
+       //.get(myapp)
+        .end(function(err,res){
+            if (err){
+		          sails.log.debug("Authorization failed"); 
+		          return done(err) 
+	          }
+            agent
+            .post('/manager/rota/add/')
+            .send(
+                 {rot_code: "zzz", rot_description: "some rota"}
+             ).expect(403)
+              .end(function(err,res){
+                  if (err){ 
+			              sails.log.debug(" there was an error adding a new rota "+err); 
+			              return done(err);
+		              }
+                done();
+             })
+        })        
+    }) 
+  }) 
+})
