210578bf24a157e119c1cb2b8f60a6b10fbf59ee
S-Stephen <stephen.shorrock@gmail.com>
2017-01-27 17:27:02 +0000

    correct end date on query

--- test/integration/controllers/Reports.test.js
+++ test/integration/controllers/Reports.test.js
@@ -15,8 +15,8 @@ describe('ManagerReportController', function() {
 		  enddate.setMonth(enddate.getMonth()+1)
       var agent =  request.agent(sails.hooks.http.app);
       Schedules.create({scd_rota_code:'HOA',scd_user_username:'fake1',scd_date:startdate}).exec(function (err,record){ if (err){ return done(err) }
-        Schedules.count({scd_date:{'>=':startdate,'<=':enddate}}).exec(function (err,found){ if (err){return done(err) }
-          sails.log.debug("number of sessins found in this month: "+found)
+        Schedules.count({scd_date:{'>=':startdate,'<':enddate}}).exec(function (err,found){ if (err){return done(err) }
+          sails.log.debug("number of sessions found in this month: "+found)
           utils.loginmanager(agent,function(err){
             utils.get(agent,'/rota/schedule/month/0',200,function (err,res){ if (err){ return done(err) }
               //res.body is array of sessions
@@ -40,8 +40,8 @@ describe('ManagerReportController', function() {
 		  enddate.setMonth(enddate.getMonth()+1)
       var agent =  request.agent(sails.hooks.http.app);
       Schedules.create({scd_rota_code:'HOA',scd_user_username:'fake1',scd_date:startdate}).exec(function (err,record){ if (err){ return done(err) }
-        Schedules.count({scd_date:{'>=':startdate,'<=':enddate}}).exec(function (err,found){ if (err){return done(err) }
-          sails.log.debug("number of sessins found in this month: "+found)
+        Schedules.count({scd_date:{'>=':startdate,'<':enddate}}).exec(function (err,found){ if (err){return done(err) }
+          sails.log.debug("number of sessions found in this month: "+found)
           utils.loginnonmanager(agent,function(err){
             utils.get(agent,'/rota/schedule/month/0',200,function (err,res){ if (err){ return done(err) }
               //res.body is array of sessions
